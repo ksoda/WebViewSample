b2052d35f12d38bf89768dc30cb675a6e2ed5cdb
Ken SONODA <ksoda@users.noreply.github.com>
2019-06-30 02:07:32 +0900

    Hello

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.webview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<WebView>(R.id.webView).loadUrl("https://example.com")
     }
 }
