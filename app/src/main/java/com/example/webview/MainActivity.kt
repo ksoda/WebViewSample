681a89a6ca3e3f07b039d8f021c54aeaaa3060d2
Ken SONODA <ksoda@users.noreply.github.com>
2019-10-22 20:31:03 +0900

    Migrate to AndroidX

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.webview
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
