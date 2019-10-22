e13301ab67d2e2d723aaa3e803ec91ab1c8e027b
Ken SONODA <ksoda@users.noreply.github.com>
2019-10-22 21:59:47 +0900

    Add samples

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.webview
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,15 +17,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val webView: WebView = findViewById(R.id.webView)
-
-        webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                return false
-            }
-        }
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = MyWebViewClient()
+        webView.addJavascriptInterface(WebAppInterface(this), "Android")
 
         // webView.loadUrl("http://10.0.2.2:5000/")
         webView.loadUrl("file:///android_asset/index.html")
 
+    }
+}
+
+class WebAppInterface(private val mContext: Context) {
+
+    /** Show a toast from the web page  */
+    @JavascriptInterface
+    fun showToast(toast: String) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
+    }
+}
+
+private class MyWebViewClient : WebViewClient() {
+
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+        if (request.url.host == "example.com") {
+            // This is my web site, so do not override; let my WebView load the page
+            return false
+        }
+
+        // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+        Intent(Intent.ACTION_VIEW, request.url).apply {
+            startActivity(view.context,this, null)
+        }
+        return true
     }
 }
